a542eedd72b1355007c5481c8ef46fb56cd1fdc0
Anthony-0908 <anthonyyuvega09@gmail.com>
2022-09-05 09:37:50 +0800

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.{html,js,php}',
+    './Admin/*.{html,js,php}',
+    './User/*.{html,js,php}',
+    './includes/*.{html,js,php}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
