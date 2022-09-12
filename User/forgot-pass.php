e36d9d545225f05a56ff7e7cab885eeb344802df
Teejhayzxc <58230971+Teejhayzxc@users.noreply.github.com>
2022-09-13 01:21:08 +0800

    tinanggal ko muna email ko

--- User/forgot-pass.php
+++ User/forgot-pass.php
@@ -28,7 +28,7 @@ require ("PHPMailer.php");
                 $mail->isSMTP();                                            //Send using SMTP
                 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                $mail->Username   = 'thaddeusgamit31@gmail.com';                     //SMTP username // email username
+                $mail->Username   = '';                     //SMTP username // email username
                 $mail->Password   = 'navydoeeuhkietor';                               //SMTP // email password password
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
                 $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
